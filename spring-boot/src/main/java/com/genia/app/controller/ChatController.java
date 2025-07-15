424db4e171e25bb2a3ea7fd6d1cc3024c9f72df4
Erik Chaupis <erik@chaupis.com>
2025-07-14 21:42:39 -0600

    Add chat in memory advisor

--- spring-boot/src/main/java/com/genia/app/controller/ChatController.java
+++ spring-boot/src/main/java/com/genia/app/controller/ChatController.java
@@ -4,19 +4,26 @@ import com.genia.app.tool.CommonTool;
 import com.genia.app.vector.LuceneVectorStore;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {"http://localhost:5173/", "https://genia-webapp.onrender.com/"})
 @Slf4j
 @RestController
 @RequestMapping("/api/chat")
@@ -28,11 +35,24 @@ public class ChatController {
     private LuceneVectorStore vectorStore;
 
     public ChatController(ChatModel chatModel, CommonTool commonTool) {
-        MethodToolCallbackProvider provider = MethodToolCallbackProvider.builder().toolObjects(commonTool).build();
-        List<ToolCallback> tools = List.of(provider.getToolCallbacks());
         chatClient = ChatClient.builder(chatModel)
-                .defaultToolCallbacks(tools)
+                .defaultToolCallbacks(getTools(commonTool))
+                .defaultAdvisors(getInMemoryChatAdvisor())
                 .build();
+    }
+
+    private List<ToolCallback> getTools(CommonTool commonTool) {
+        MethodToolCallbackProvider provider = MethodToolCallbackProvider.builder().toolObjects(commonTool).build();
+        return List.of(provider.getToolCallbacks());
+    }
+
+    private MessageChatMemoryAdvisor getInMemoryChatAdvisor() {
+        ChatMemoryRepository repository = new InMemoryChatMemoryRepository();
+        ChatMemory chatMemory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(repository)
+                .maxMessages(10)
+                .build();
+        return MessageChatMemoryAdvisor.builder(chatMemory).build();
     }
 
     @PostMapping
@@ -50,7 +70,16 @@ public class ChatController {
 
     @PostMapping("/rag")
     public String rag(@RequestBody String question) {
-        PromptTemplate domainTemplate = PromptTemplate.builder().template("""
+        Prompt prompt = getDomainTemplate().create(Map.of("question", question));
+        String response = chatClient.prompt(prompt)
+                .advisors(new QuestionAnswerAdvisor(vectorStore))
+                .call()
+                .content();
+        return response;
+    }
+
+    private PromptTemplate getDomainTemplate() {
+        return PromptTemplate.builder().template("""
                 You are a legal assistant specialized in US labor laws.
                 Use the following context to answer the question precisely.
                 If the answer is not in the context, say "I don't know". 
@@ -61,11 +90,5 @@ public class ChatController {
 
                 Answer:
                 """).build();
-        Prompt prompt = domainTemplate.create(Map.of("question", question));
-        String response = chatClient.prompt(prompt)
-                .advisors(new QuestionAnswerAdvisor(vectorStore))
-                .call()
-                .content();
-        return response;
     }
 }
